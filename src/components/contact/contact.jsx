2d0591ea2b7f8b19c7e8a5380782f87a3b70ff6c
marcusbyui <ojo18002@byui.edu>
2024-07-16 16:07:12 +0200

    update email

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -6,29 +6,33 @@ const Contact = () => {
     const list = useMemo(() => [
         {
             img: "whatsapp.png",
-            text: "Chat Us On WhatsApp"
+            text: "Chat Us On WhatsApp",
+            link: "https://wa.link/j607ga"
         },
         {
             img: "messenger.png",
-            text: "Chat Us On Messenger"
+            text: "Chat Us On Messenger",
+            link: "https://www.facebook.com/people/Little-Explorers/61562028847692"
         },
         {
             img: "email.png",
-            text: "Send An Email"
+            text: "Send An Email",
+            link: "mailto:hello@littleexplorers.rw"
         },
         {
             img: "call.png",
-            text: "Call In"
+            text: "Call In",
+            link: "tel:+250794778307"
         },
 
     ], [])
     return (
         <div className={styles.contact}>
-            <p>No 2, KG 327. Behind MTN Center. Nyarutarama, Kigali, Rwanda</p>
+            <p>No 2, KG 327. Behind MTN Center. Nyarutarama, Kigali, Rwanda</p>
             <h2>Needs Help? Let’s Get in Touch</h2>
             <div>
                 {list.map((item, index) => {
-                    return <div key={index} className={styles.item}>
+                    return <div onClick={()=>window.open(item.link)} key={index} className={styles.item}>
                         <img src={getImageUrl(item.img)} alt={item.text} />
                         <span>{item.text}</span>
                     </div>
